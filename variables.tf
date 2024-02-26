9a7045533551bdc8ebd26a75f71ac5c12714e61e
davidampaire <58703282+davidampaire@users.noreply.github.com>
2024-02-26 19:22:16 +0300

    Refactor to use variables

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "instance_type" {
 }
 
 variable "ami_filter" {
-  description = "Name filtr and owner for AMI"
+  description = "Name filter and owner for AMI"
 
   type = object ({
     name = string
@@ -12,8 +12,8 @@ variable "ami_filter" {
   })
 
   default = {
-    name   = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-    owners = "979382823631" # Bitnami
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
   }
 }
 
@@ -21,82 +21,35 @@ data "aws_vpc" "default" {
   default = "true"
 }
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+variable "environment" {
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  description = "development environment"
 
+  type = object ({
+    name           = string
+    network_prefix = string
+  })
+
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+  }
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
 
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
-module "autoscaling" {
-  source   = "terraform-aws-modules/autoscaling/aws"
-  version  = "7.4.0"
-  name     = "blog"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
-
-  image_id        = data.aws_ami.app_ami.id
-  instance_type   = var.instance_type
+variable "asg_min_size" {
+  description = "minimum number of instances in the ASG"
+  default = 1
 }
 
-module "blog_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
-
-  name = "blog-alb"
-
-  load_balancer_type = "application"
-
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
-
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-    }
-  ]
-
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  tags = {
-    Environment = "dev"
-  }
-}
-
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  
-  name = "blog"
-
-  vpc_id      = module.blog_vpc.vpc_id
-
-  ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+variable "asg_max_size" {
+  description = "Maximum number of instances in the ASG"
+  default = 2
 }
